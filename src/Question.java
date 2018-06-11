997337462c31f174812236c41ebd5e11333b2300
scacio <andrea.caciolai@gmail.com>
2018-06-11 13:50:24 +0200

    Timer
    Aggiunto un timer di 30 minuti per rendere più fedele la simulazione d'esame

--- src/Question.java
+++ src/Question.java
@@ -1,24 +1,28 @@
 public class Question {
-  private String question;
-  private String[] answers;
-  private char rightAnswer;
+    private String question;
+    private String[] answers;
+    private char rightAnswer;
 
-  public Question(String question, String[] answers) {
-    this.question = question;
-    this.answers = answers;
-    this.rightAnswer = ' ';
-  }
+    public Question(String question, String[] answers) {
+        this.question = question;
+        this.answers = answers;
+        this.rightAnswer = ' ';
+    }
 
-  public String getQuestion() {
-    return question;
-  }
+    public String getQuestion() {
+        return question;
+    }
 
-  public String[] getAnswers() { return answers; }
+    public String[] getAnswers() {
+        return answers;
+    }
 
-  public char getRightAnswer() { return rightAnswer; }
+    public char getRightAnswer() {
+        return rightAnswer;
+    }
 
-  public void setRightAnswer(char rightAnswer) {
-    this.rightAnswer = rightAnswer;
-  }
+    public void setRightAnswer(char rightAnswer) {
+        this.rightAnswer = rightAnswer;
+    }
 
 }
